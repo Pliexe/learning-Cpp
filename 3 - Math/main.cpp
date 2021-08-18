cf0a79896b706ed53b1507aa0c3d76c86a6fac07
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2021-08-19 00:41:26 +0200

    d

---
+++ 3 - Math/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << pow(3, 6) << std::endl;
+
+    printf("Testing k\n");
+
+    std::cout << ceil(3.1) << std::endl;
+    std::cout << floor(3.1) << std::endl;
+
+    std::cout << fmax(3, 10) << std::endl;
+    std::cout << fmin(3, 10) << std::endl;
+
+    return 0;
+}
